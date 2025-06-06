d2a4e72ef96d3279481d858e0065f04699336ce0
Fakhri Abdillah <fakhriabdull03@gmail.com>
2025-06-06 19:36:10 +0700

    Menambahkan Top App Bar

--- app/src/main/java/com/fakhri0079/cineshelf2/MainActivity.kt
+++ app/src/main/java/com/fakhri0079/cineshelf2/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.fakhri0079.cineshelf2
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.fakhri0079.cineshelf2.ui.theme.CineShelf2Theme
 
@@ -19,15 +25,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CineShelf2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello Android",
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -39,9 +69,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     CineShelf2Theme {
-        Greeting("Android")
+        MainScreen()
     }
 }
