b25fc6137a19807eeb303117b43b9600ce5b2746
Fakhri Abdillah <fakhriabdull03@gmail.com>
2025-06-12 00:21:37 +0700

    Membuat indikator loading image

--- app/src/main/java/com/fakhri0079/cineshelf2/ui/screen/MainScreen.kt
+++ app/src/main/java/com/fakhri0079/cineshelf2/ui/screen/MainScreen.kt
@@ -8,15 +8,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -44,11 +45,11 @@ import com.fakhri0079.cineshelf2.ui.theme.CineShelf2Theme
 fun MainScreen() {
     Scaffold(
         topBar = {
-            TopAppBar(
+            CenterAlignedTopAppBar(
                 title = {
                     Text(text = stringResource(id = R.string.app_name))
                 },
-                colors = TopAppBarDefaults.topAppBarColors(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 )
@@ -64,9 +65,9 @@ fun MainScreen() {
 fun ScreenContent(modifier: Modifier = Modifier) {
     val viewModel: MainViewModel = viewModel()
     val data by viewModel.data
-    LazyVerticalGrid(
+    LazyVerticalStaggeredGrid(
         modifier = modifier.fillMaxSize().padding(4.dp),
-        columns = GridCells.Fixed(2)
+        columns = StaggeredGridCells.Fixed(2)
     ) {
         items(data){
             ListItem(cinema = it)
@@ -87,6 +88,7 @@ fun ListItem(cinema: Cinema) {
                 .build(),
             contentDescription = stringResource(id = R.string.gambar, cinema.nama),
             contentScale = ContentScale.Crop,
+            placeholder = painterResource(id = R.drawable.loading_img),
             modifier = Modifier.fillMaxWidth().padding(4.dp).border(1.dp, Color.Unspecified,shape = RoundedCornerShape(10.dp))
         )
         Column(
