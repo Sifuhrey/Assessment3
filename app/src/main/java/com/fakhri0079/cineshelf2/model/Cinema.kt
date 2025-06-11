2400898e90d05d276a7715570cd90c0dbf467c56
Fakhri Abdillah <fakhriabdull03@gmail.com>
2025-06-11 19:45:38 +0700

    Memproses data string sebagai JSON

---
+++ app/src/main/java/com/fakhri0079/cineshelf2/model/Cinema.kt
@@ -0,0 +1,7 @@
+package com.fakhri0079.cineshelf2.model
+
+data class Cinema(
+    val nama: String,
+    val namaLatin: String,
+    val imageId: String
+)
--- app/src/main/java/com/fakhri0079/cineshelf2/network/CinemaApiService.kt
+++ app/src/main/java/com/fakhri0079/cineshelf2/network/CinemaApiService.kt
@@ -1,18 +1,25 @@
 package com.fakhri0079.cineshelf2.network
 
+import com.fakhri0079.cineshelf2.model.Cinema
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://raw.githubusercontent.com/indraazimi/mobpro1-compose/static-api/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 interface CinemaApiService {
     @GET("static-api.json")
-    suspend fun getCinema(): String
+    suspend fun getCinema(): List<Cinema>
 }
 
 object CinemaApi{
