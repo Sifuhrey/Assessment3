a6f0bf27a2bc5b25c04e8e5ed94991ae664b4d6a
Fakhri Abdillah <fakhriabdull03@gmail.com>
2025-06-11 23:18:07 +0700

    Membuat tampilan nama film

--- app/src/main/java/com/fakhri0079/cineshelf2/ui/screen/MainScreen.kt
+++ app/src/main/java/com/fakhri0079/cineshelf2/ui/screen/MainScreen.kt
@@ -1,8 +1,10 @@
 package com.fakhri0079.cineshelf2.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -18,13 +20,17 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -71,7 +77,8 @@ fun ScreenContent(modifier: Modifier = Modifier) {
 @Composable
 fun ListItem(cinema: Cinema) {
     Box(
-        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray, shape = RoundedCornerShape(6.dp))
+        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray, shape = RoundedCornerShape(6.dp)),
+        contentAlignment = Alignment.BottomCenter
     ) {
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
@@ -82,6 +89,23 @@ fun ListItem(cinema: Cinema) {
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxWidth().padding(4.dp).border(1.dp, Color.Unspecified,shape = RoundedCornerShape(10.dp))
         )
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(4.dp)
+                .background(Color(red = 0f, green = 0f, blue = 0f, alpha = 0.5f))
+                .padding(4.dp)
+        ) {
+            Text(
+                text = cinema.nama,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Text(
+                text = cinema.namaLatin,
+                fontStyle = FontStyle.Italic,
+                fontSize = 14.sp,
+                color = Color.White
+            )
+        }
     }
 }
 
